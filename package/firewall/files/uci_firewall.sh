maxrio/openwrt

commit 71394ccb5e8adc47f44414e5e2b5f354829d7f68

firewall: do not process rules in reverse
git-svn-id: svn://svn.openwrt.org/openwrt/trunk@18015 3c298f89-4303-0410-b956-a3cf2f4a3e73

package/firewall/files/uci_firewall.sh
@@ -280,7 +280,7 @@ fw_rule() {
 	[ -n "$src" -a -n "$dest" ] && ZONE=zone_${src}_forward
 	[ -n "$dest" ] && TARGET=zone_${dest}_$target
 	add_rule() {
-		$IPTABLES -I $ZONE 1 \
+		$IPTABLES -A $ZONE \
 			${proto:+-p $proto} \
 			${icmp_type:+--icmp-type $icmp_type} \
 			${src_ip:+-s $src_ip} \

commit 0244a82c5e998c90bdce03902ac39769a8bb37b6

[package] firewall: insert rules at the beginning of chains again while maintaining non reversed order, fixes wrong ordering introduced by r18015

package/firewall/files/uci_firewall.sh
@@ -294,8 +294,11 @@ fw_rule() {
 	[ -n "$src" -a -z "$dest" ] && ZONE=zone_$src
 	[ -n "$src" -a -n "$dest" ] && ZONE=zone_${src}_forward
 	[ -n "$dest" ] && TARGET=zone_${dest}_$target
+
+	eval 'RULE_COUNT=$((++RULE_COUNT_'$ZONE'))'
+
 	add_rule() {
-		$IPTABLES -A $ZONE \
+		$IPTABLES -I $ZONE $RULE_COUNT \
 			${proto:+-p $proto} \
 			${icmp_type:+--icmp-type $icmp_type} \
 			${src_ip:+-s $src_ip} \