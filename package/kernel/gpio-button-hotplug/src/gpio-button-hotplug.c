maxrio/openwrt

commit 00759f796b5f80dbeee615d2f16f9fa4e63b3161

gpio-button-hotplug: use gpio_button_get_value() to initialize last_state.
TL-WR720N-v3 has a slider switch composed of 2 GPIO buttons which can be
used to swtich between 3 positions.  At leat 1 button is in pressed state
in any of those positions.  Initialize 'last_state' as 0 (released) will
cause the device to automatically enter failsafe mode on every bootup.

Signed-off-by: Yousong Zhou <yszhou4tech@gmail.com>

git-svn-id: svn://svn.openwrt.org/openwrt/trunk@37643 3c298f89-4303-0410-b956-a3cf2f4a3e73

package/kernel/gpio-button-hotplug/src/gpio-button-hotplug.c
@@ -509,7 +509,7 @@ static int gpio_keys_polled_probe(struct platform_device *pdev)
 		}
 
 		bdata->can_sleep = gpio_cansleep(gpio);
-		bdata->last_state = 0;
+		bdata->last_state = gpio_button_get_value(button, bdata);
 		bdata->threshold = DIV_ROUND_UP(button->debounce_interval,
 						pdata->poll_interval);
 	}

commit 0cccc2a5eb9b223c9514ff6024f51d17b6f6c099

gpio-button-hotplug: fix active_low handling, possibly broken in r37643
Signed-off-by: Felix Fietkau <nbd@openwrt.org>

package/kernel/gpio-button-hotplug/src/gpio-button-hotplug.c
@@ -274,23 +274,24 @@ struct gpio_keys_polled_dev {
 	struct gpio_keys_button_data data[0];
 };
 
-static inline int gpio_button_get_value(struct gpio_keys_button *button,
-			  struct gpio_keys_button_data *bdata)
+static int gpio_button_get_value(struct gpio_keys_button *button,
+				 struct gpio_keys_button_data *bdata)
 {
+	int val;
+
 	if (bdata->can_sleep)
-		return !!gpio_get_value_cansleep(button->gpio);
+		val = !!gpio_get_value_cansleep(button->gpio);
 	else
-		return !!gpio_get_value(button->gpio);
+		val = !!gpio_get_value(button->gpio);
+
+	return val ^ button->active_low;
 }
 
 static void gpio_keys_polled_check_state(struct gpio_keys_button *button,
 					 struct gpio_keys_button_data *bdata)
 {
-	int state;
+	int state = gpio_button_get_value(button, bdata);
 
-	state = gpio_button_get_value(button, bdata);
-
-	state = !!(state ^ button->active_low);
 	if (state != bdata->last_state) {
 		unsigned int type = button->type ?: EV_KEY;
 